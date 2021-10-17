0eb9a539e2027145817a180e011405b3faea69b5
Nikhil Kale <nikhilkale903@gmail.com>
2021-10-17 12:32:07 +0530

    Case 1 completed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,19 @@
 package calculator;
 
-class StringCalculator {
+public class StringCalculator {
 
-    public int add(String input) {
-        return 0;
-    }
+	public int add(String input) {
+
+		if (input == null || input.length() == 0) {
+			return 0;
+		}
+        String s="\n,";
+        String str[]= s.split(s);
+        int sum=0;
+        for(int i=0;i<s.length();i++) {
+            sum+=Integer.parseInt(str[i]));
+        }
+        return sum;
+	}
 
 }
